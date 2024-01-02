70b267c8fe4b0b9bb0768cefe8348bc201bf8102
unknown <leonardovwes@gmail.com>
2024-01-02 09:01:28 -0300

    atualização 020124

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit{
+
+  @Input() photoCover:string = ""
+  @Input() cardTitle:string =""
+  @Input() cardDescription:string =""
+  @Input() id:string ="0"
+
+
+  constructor(){}
+  ngOnInit(): void {
+
+  }
 
 }
